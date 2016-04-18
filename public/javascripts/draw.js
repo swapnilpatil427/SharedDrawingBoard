0fbe2443b6945a8ed611c5ca40090b83fe0a5e4b
swapnil <swapnilpatil427@gmail.com>
2016-04-17 18:02:03 -0700

    Chat rooms socket problem fixed

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -170,3 +170,19 @@ function clearCanvas() {
             end_external_path(JSON.parse(data), sessionId);
         }
     });
+
+    $("#send").click(function() {
+        socket.emit('chat', $("#message").val(),room);
+        $("#message").val('');
+        return false;
+    });
+    socket.on('chat', function(msg) {
+        var date = new Date($.now());
+        var time = date.getHours() + ":" + date.getMinutes();
+        $('#chatbox').append($('<hr><div class="row"><div class="col-lg-12"><div class="media"><a class="pull-left" href="#">' +
+            '<img class="media-object img-circle" src="/icon/user.png" alt=""></a><div class="media-body">' +
+            '<h4 class="media-heading">User<span class="small pull-right">' + time + '</span></h4><p style=" word-wrap: break-word; width:200px">' + msg +
+            '</p></div></div></div></div>'));
+        m = document.getElementById('chatbox');
+        m.scrollTop = m.offsetHeight;
+    });
