3a140329c5bba18ffeb08c81efc78818b408511d
swapnil <swapnilpatil427@gmail.com>
2016-05-07 15:15:53 -0700

    testing statement removed

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var draw = require('./controllers/drawController');
 var fs = require('fs')
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
